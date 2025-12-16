affd22ccb67611e9fa7223b6c312aaa2484ea6e4
KomalSonawane15699 <komalsonawane15699@gmail.com>
2025-12-16 10:00:38 +0530

    push changes

--- online-quiz/src/components/Header/Header.js
+++ online-quiz/src/components/Header/Header.js
@@ -1,22 +1,54 @@
-// src/components/Header/Header.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import SignupPage from '../../pages/signupPage/SignupPage';
+import LoginPage from '../../pages/loginPage/LoginPage';
 
-function Header() {
-    return (
-        <header className="landing-header">
-            <div className="logo">Quizzy</div>
-            <nav>
-                <a href="#home">Home</a>
-                <a href="#subjects">Subjects</a>
-                <a href="#features">Features</a>
-                <a href="#contact">Contact</a>
-                <button className="btn-secondary nav-button">Sign In</button>
-                <button className="btn-primary nav-button">Sign Up</button>
-            </nav>
-        </header>
-    );
+function Header({ onSignupClick, onLoginClick }) {
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const handleSignupClick = () => {
+    setShowSignupModal(true);
+    setShowLoginModal(false);
+  };
+
+  const handleLoginClick = () => {
+    setShowLoginModal(true);
+    setShowSignupModal(false);
+  };
+
+  const handleCloseModal = () => {
+    setShowSignupModal(false);
+    setShowLoginModal(false);
+  };
+
+  return (
+    <header className="landing-header">
+      <div className="logo">Quizzy</div>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#subjects">Subjects</a>
+        <a href="#features">Features</a>
+        <a href="#contact">Contact</a>
+        <button className="btn-primary nav-button" onClick={handleLoginClick}>Sign In</button>
+        <button className="btn-secondary nav-button" onClick={handleSignupClick}>Sign Up</button>
+      </nav>
+      {showSignupModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <SignupPage onSignupClick={onSignupClick} onLoginClick={onLoginClick}  onNavigate={handleCloseModal} />
+          </div>
+        </div>
+      )}
+      {showLoginModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <LoginPage onSignupClick={onSignupClick} onLoginClick={onLoginClick} onNavigate={handleCloseModal} />
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
